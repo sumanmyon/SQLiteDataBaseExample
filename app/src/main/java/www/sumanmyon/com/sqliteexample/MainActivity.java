cd79d0ff962e52a5e8448a197da91418ed314697
Suman Neupane <sumanmyon.sam@gmail.com>
2019-04-05 11:26:15 +0545

    Casting edittext and button in Activity

--- app/src/main/java/www/sumanmyon/com/sqliteexample/MainActivity.java
+++ app/src/main/java/www/sumanmyon/com/sqliteexample/MainActivity.java
@@ -2,10 +2,15 @@ package www.sumanmyon.com.sqliteexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     DataBaseHelper myDB;
+
+    EditText editTextName, editTextSurName, editTextMarks;
+    Button buttonInsert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDB = new DataBaseHelper(this);
+
+        castingViews();
+    }
+
+    private void castingViews() {
+        editTextName = (EditText)findViewById(R.id.edit_text_name);
+        editTextSurName = (EditText)findViewById(R.id.edit_text_surname);
+        editTextMarks = (EditText)findViewById(R.id.edit_text_marks);
+        buttonInsert = (Button) findViewById(R.id.button_insert);
     }
 }
